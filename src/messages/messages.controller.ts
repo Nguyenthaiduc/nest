c0e9672da003e4bfcd02d8847d53f3235957c745
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-21 16:15:47 +0700

    exception

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,17 +1,23 @@
 /* eslint-disable */
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message-dto';
 import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-  messagesService : MessagesService;
+  messagesService: MessagesService;
 
   constructor() {
     //DON'T DO THIS ON REAL APP
     //USE DEPENDENCIES INJECTION
     this.messagesService = new MessagesService();
-
   }
 
   @Get()
@@ -21,12 +27,16 @@ export class MessagesController {
   @Post()
   createMessages(@Body() body: CreateMessageDto) {
     return this.messagesService.create(body.content);
-    
   }
 
   @Get('/:id')
-  getMessages(@Param() id: string) {
-    return this.messagesService.findOne(id);
-    
+  async getMessages(@Param() id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message is not found');
+    }
+
+    return message;
   }
 }
