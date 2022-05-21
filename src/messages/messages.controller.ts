4037223514abff9ae14976cb3c119bd703ac7562
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-21 16:10:16 +0700

    test http

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,17 +1,32 @@
 /* eslint-disable */
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message-dto';
+import { MessagesService } from './messages.service';
+
 @Controller('messages')
 export class MessagesController {
+  messagesService : MessagesService;
+
+  constructor() {
+    //DON'T DO THIS ON REAL APP
+    //USE DEPENDENCIES INJECTION
+    this.messagesService = new MessagesService();
+
+  }
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
   @Post()
   createMessages(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
+    
   }
 
   @Get('/:id')
   getMessages(@Param() id: string) {
-    console.log(id);
+    return this.messagesService.findOne(id);
+    
   }
 }
