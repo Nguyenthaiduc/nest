9f3c82d28a1042dd488d0c44edec012ce1f884cc
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-24 17:55:25 +0700

    set up database sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { ReportsModule } from './reports/reports.module';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+    ReportsModule,
+    UsersModule,
+  ],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
