6c557c79c45f7ae6a712d45b98ad84568ede51ff
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-25 10:04:50 +0700

    008 Setting Up Body Validation

---
+++ src/users/dtos/create-user.dtop.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import { IsEmail, IsString } from 'class-validator';
+export class CreateUserDto {
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    password: string;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable */
+import { Body } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dtop';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+      console.log(body);
+  }
+}
