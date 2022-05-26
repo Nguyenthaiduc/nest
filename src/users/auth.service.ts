09ccf806599597a8e57ed80adadd9e91f3114ae2
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-27 06:53:50 +0700

    See if email its in use

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,8 +1,26 @@
 /* eslint-disable */
-import { Injectable } from '@nestjs/common';
-import { UserService } from './users.service';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UserService) {}
+    constructor(private usersService: UsersService) {}
+
+    async signup(email: string, password: string){
+        // See if email its in use
+        const users = await this.usersService.find(email);
+        if(users.length) {
+            throw new BadRequestException('Email is use');
+        }
+
+        // Hash the users password
+
+        // Create a new user and save it
+
+        // return the user 
+    }
+
+    signin() { 
+
+    }
 }
