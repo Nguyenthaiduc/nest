a57295d67f98c2a8aa2c2c27d2431fdea3fb5872
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-27 08:36:30 +0700

    Changing and Fetching Session Data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
   Delete,
   NotFoundException,
+  Session,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -25,6 +26,16 @@ export class UsersController {
       private usersService: UsersService,
       private authService : AuthService,
       ) {}
+
+  // @Get('/colors/:color')
+  // setColors(@Param('color') color: string, @Session() session: any) {
+  //   session.color = color;
+  // }
+
+  // @Get('/colors')
+  // getColor(@Session() session: any) {
+  //   return session.color;
+  // }
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
