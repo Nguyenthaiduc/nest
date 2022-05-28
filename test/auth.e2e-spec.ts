d96d5b4395e90d4af7d64ccf45a1c73a82161e9e
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-28 14:25:20 +0700

    A Followup Test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -43,4 +43,22 @@ describe('Authencation System', () => {
         expect(email).toEqual(email);
       });
   });
+
+  it('signup as a new user then get the currently logged in user',async ()=> {
+    const email = 'abc@gmail.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: '123456' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie',cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
