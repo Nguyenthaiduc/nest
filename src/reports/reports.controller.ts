9b584f352817b53a498476cf9921740370c720fb
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-28 17:28:36 +0700

    The Guard Doesn't Work_!

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -15,6 +15,7 @@ import { User } from 'src/users/user.entity';
 import { ReportDto } from './dtos/report.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptors';
 import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from 'src/guards/admin.guard'; 
 
 @Controller('reports')
 export class ReportsController {
@@ -28,6 +29,7 @@ export class ReportsController {
   }
 
   @Patch('/:id')
+  @UseGuards(AdminGuard)
   approveReport(@Param('id') id : string, @Body() body : ApproveReportDto) {
     return this.reportsService.changeApproval(id,body.approved);
 
