e7828b5ee6c18c40ea281d3e56def3f3a3ad1716
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-28 18:09:03 +0700

    Transforming Query String Data

--- src/reports/dtos/get-estimate.dto.ts
+++ src/reports/dtos/get-estimate.dto.ts
@@ -8,6 +8,7 @@ import {
     IsLongitude,
     IsLatitude,
  } from 'class-validator';
+ import { Transform } from 'class-transformer';
 
 export class GetEstimateDto {
     @IsString()
@@ -16,19 +17,23 @@ export class GetEstimateDto {
     @IsString()
     model: string;
 
+    @Transform(({ value })=> parseInt(value)) // convert data => number
     @IsNumber()
     @Min(1930)
     @Max(2050)
     year: number;
 
+    @Transform(({ value })=> parseInt(value))
     @IsNumber()
     @Min(0)
     @Max(1000000)
     mileage: number;
 
+    @Transform(({ value })=> parseFloat(value))
     @IsLongitude()
     lng: number;
 
+    @Transform(({ value })=> parseFloat(value))
     @IsLatitude()
     lat: number;
 
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -27,7 +27,7 @@ export class ReportsController {
 
   @Get()
   getEstimate(@Query() query: GetEstimateDto) {
-    
+      console.log(query);
   }
 
   @Post()
