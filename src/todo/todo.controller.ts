fd61a2b8aea33751daf9219f2c0e7ed8e41f862f
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-06-06 21:18:32 +0700

    update

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,5 +1,15 @@
 /* eslint-disable */
-import { Controller,Body,Post,Get,Patch,Delete,Query,Param,UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Query,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dtos/create-todo.dto';
 import { Todo } from './todo.entity';
@@ -8,39 +18,43 @@ import { UpdateTodoDto } from './dtos/update-todo.dto';
 
 @Controller('todo')
 export class TodoController {
-    constructor(private todoService: TodoService) { }
+  constructor(private todoService: TodoService) {}
 
-    //add a todo
-    @Post()
-    @UseGuards(AuthGuard)
-    async createTodo(@Body() body : CreateTodoDto) {
-        return await this.todoService.create(body);
-    }
-    //get all todo
-    @Get()
-    async getAllTodos(@Query() query : CreateTodoDto) {
-        return await this.todoService.find(query);
-    }
-    //get a todo
-    @Get('/:id')
-    async getTodo(@Param('id') id: string): Promise<Todo> {
-        return await this.todoService.findOne(parseInt(id));
-    }
-    //get a todo by title
-    @Get('/title/:title')
-    async getTodoByTitle(@Param('title') title: string): Promise<Todo[]> {
-        return await this.todoService.findTitle(title);
-    }
-    //update a todo
-    @Patch('/:id')
-    @UseGuards(AuthGuard)
-    async updateTodo(@Param('id') id : string,@Body() body : UpdateTodoDto): Promise<Todo> {
-        return await this.todoService.update(parseInt(id),body);
-    }
-    //delete a todo
-    @Delete('/:id')
-    @UseGuards(AuthGuard)
-    async deleteTodo(@Param('id') id : string): Promise<Todo> {
-        return await this.todoService.remove(parseInt(id));
-    }
+  //add a todo
+  @Post()
+  @UseGuards(AuthGuard)
+  async createTodo(@Body() body: CreateTodoDto) {
+    return await this.todoService.create(body);
+  }
+  //get all todo
+  @Get()
+  async getAllTodos() {
+    console.log(this.todoService.find());
+    return await this.todoService.find();
+  }
+  //get a todo
+  @Get('/:id')
+  async getTodo(@Param('id') id: string): Promise<Todo> {
+    return await this.todoService.findOne(parseInt(id));
+  }
+  //get a todo by title
+  @Get('/title/:title')
+  async getTodoByTitle(@Param('title') title: string): Promise<Todo[]> {
+    return await this.todoService.findTitle(title);
+  }
+  //update a todo
+  @Patch('/:id')
+  @UseGuards(AuthGuard)
+  async updateTodo(
+    @Param('id') id: string,
+    @Body() body: { title?: string; completed?: boolean; description?: string },
+  ): Promise<Todo> {
+    return await this.todoService.update(parseInt(id), body);
+  }
+  //delete a todo
+  @Delete('/:id')
+  @UseGuards(AuthGuard)
+  async deleteTodo(@Param('id') id: string): Promise<Todo> {
+    return await this.todoService.remove(parseInt(id));
+  }
 }
